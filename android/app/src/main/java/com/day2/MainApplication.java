ff2e20f39e24902c0ac233c66eb30e5e5a5c6a55
ahmedelnoamany <ahmedgouda9484@gmail.com>
2018-11-26 15:12:02 -0600

    Installed swiper, vector icons and redux packages.

--- android/app/src/main/java/com/day2/MainApplication.java
+++ android/app/src/main/java/com/day2/MainApplication.java
@@ -3,6 +3,9 @@ package com.day2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNNotificationsPackage(),
+            new LinearGradientPackage(),
             new SvgPackage()
       );
     }
